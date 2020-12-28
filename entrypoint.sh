032862f25967c7d7a956d777d5e79155fbbc353e
aurbar <aurelien@barbier-accary.info>
2020-12-28 16:02:24 +0100

    parameterization

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -l
 
-#git ftp push --force \
 git ftp push \
-  --auto-init \
   --syncroot $INPUT_SYNCROOT \
   --user $INPUT_USER \
   --passwd $INPUT_PASSWORD \
+  $INPUT_OPTIONS \
   $INPUT_URL
